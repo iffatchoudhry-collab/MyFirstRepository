6624c5ee25c3a16c60c4a86f00ea2a6fa775acd8
iffatchoudhry-collab <iffat.choudhry@gmail.com>
2026-09-23 22:59:41 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+first_number= int(input("Please Enter first number: "))
+second_number= int(input("Please enter second number: "))
+sum=first_number+second_number
+subtraction=first_number-second_number
+multiplication=first_number*second_number
+division=first_number/second_number
+print("sum is",sum)
+print("Subtraction is",subtraction)
+print("Multiplication is",multiplication)
+print("Division is",division)
